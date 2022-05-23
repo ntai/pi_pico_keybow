48e788b37edfc5ba0b22cc9ed65d3d987063ae94
Naoyuki Tai <ntai@cleanwinner.com>
2022-05-23 19:35:55 -0400

    Reactive 0-F.

---
+++ examples/keybow_basic.py
@@ -0,0 +1,95 @@
+#
+import math
+from keybow2040 import Keybow2040, number_to_xy, hsv_to_rgb
+# from keybow_hardware.pim56x import PIM56X as Hardware # for Keybow 2040
+from keybow_hardware.pim551 import PIM551 as Hardware # for Pico RGB Keypad Base
+
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+# Set up Keybow
+keybow = Keybow2040(Hardware())
+
+keys = [None] * 16
+for index in range(16):
+    rearranged = 3 - (math.floor(index / 4)) + ((index % 4) * 4)
+    keys[index] = keybow.keys[rearranged]
+
+keyboard = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(keyboard)
+
+SHORTS = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  ]
+
+MAP = {}
+
+for index in range(16):
+    MAP[keys[index]] = SHORTS[index]
+    def press_handler(key):
+        layout.write(MAP[key])
+    keybow.on_press(keys[index], handler=press_handler)
+
+
+def rainbow(step):
+    for i in range(16):
+        # Convert the key number to an x/y coordinate to calculate the hue
+        # in a matrix style-y.
+        x, y = number_to_xy(i)
+
+        # Calculate the hue.
+        hue = (x + y + (step / 20)) / 8
+        hue = hue - int(hue)
+        hue = hue - math.floor(hue)
+
+        # Convert the hue to RGB values.
+        r, g, b = hsv_to_rgb(hue, 1, 1)
+
+        # Display it on the key!
+        keys[i].set_led(r, g, b)
+
+
+#
+for index in range(0, 64):
+    rainbow(index)
+
+#
+pressed_color = (0, 255, 255)
+black = (0, 0, 0)
+
+def reactive():
+    # Loop through the keys and set the LED to cyan if pressed, otherwise turn
+    # it off (set it to black).
+    for key in keys:
+        if key.pressed:
+            key.set_led(*pressed_color)
+        else:
+            key.set_led(*black)
+
+# Increment step to shift animation across keys.
+step = 0
+
+while True:
+    # Always remember to call keybow.update() on every iteration of your loop!
+    keybow.update()
+
+    step += 1
+    # rainbow(step)
+    reactive()
